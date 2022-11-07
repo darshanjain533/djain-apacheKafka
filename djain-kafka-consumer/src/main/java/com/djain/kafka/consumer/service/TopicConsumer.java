96ff71069363f300bd172931cfc2d4a577a220f3
darshanjain533 <darshanjain533@gmail.com>
2022-11-07 06:44:12 +0530

    latest code commit

--- djain-kafka-consumer/src/main/java/com/djain/kafka/consumer/service/TopicConsumer.java
+++ djain-kafka-consumer/src/main/java/com/djain/kafka/consumer/service/TopicConsumer.java
@@ -2,6 +2,7 @@ package com.djain.kafka.consumer.service;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
@@ -13,9 +14,10 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class TopicConsumer {
 	
-	@Value("${topic.name.consumer")
+	@Value("${topic.name.consumer}")
     private String topicName;
 	
+	@KafkaListener(topics = "#{'${topic.name.consumer}'}", groupId = "#{'${spring.kafka.consumer.group-id}'}")
 	public void consume(ConsumerRecord<String, String> payload) {
 		log.info("Topic is: {}", topicName);
         log.info("key is: {}", payload.key());
@@ -24,3 +26,46 @@ public class TopicConsumer {
         log.info("Order is: {}", payload.value());
 	}
 }
+
+
+/*
+	For a topic with multiple partitions, however, a @KafkaListener can explicitly subscribe to a particular partition of a topic with an initial offset
+	 @KafkaListener(
+		  topicPartitions = @TopicPartition(topic = "topicName",
+		  partitionOffsets = {
+		    @PartitionOffset(partition = "0", initialOffset = "0"), 
+		    @PartitionOffset(partition = "3", initialOffset = "0")}),
+		  	containerFactory = "partitionsKafkaListenerContainerFactor"
+		 )
+ */
+
+
+
+/*
+	We can configure listeners to consume specific message content by adding a custom filter. This can be done by setting a RecordFilterStrategy to the KafkaListenerContainerFactory:
+	
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, String> CustomKafkaFilter() {
+	
+	    ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+	    factory.setConsumerFactory(consumerFactory());
+	    factory.setRecordFilterStrategy(
+	      record -> record.value().contains("World"));
+	    
+	    return factory;
+	}
+
+	We can then configure a listener to use this container factory:
+	
+	@KafkaListener(
+	  topics = "topicName", 
+	  containerFactory = "CustomKafkaFilter")
+	public void listenWithFilter(String message) {
+	    System.out.println("Received Message in filtered listener: " + message);
+	}
+
+*/
+
+
+
+
--- djain-kafka-producer/src/main/java/com/djain/kafka/producer/service/TopicProducer.java
+++ djain-kafka-producer/src/main/java/com/djain/kafka/producer/service/TopicProducer.java
@@ -2,8 +2,11 @@ package com.djain.kafka.producer.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
-import lombok.AllArgsConstructor;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,13 +15,34 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class TopicProducer {
 	
-	private final KafkaTemplate<String, String> kafkatemplate;
+	private final KafkaTemplate<String, String> kafkaTemplate;
 	
 	@Value("${topic.name.producer}")
     private String topicName;
 	
 	public void send(String msg) {
 		log.info("Inside Producer Send service");
-		kafkatemplate.send(topicName, msg);
+		//kafkatemplate.send(topicName, msg);
+		
+		ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(topicName, msg);
+
+		future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+			@Override
+			public void onSuccess(SendResult<String, String> result) {
+				System.out.println("Sent message=[" + msg + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+			}
+
+			@Override
+			public void onFailure(Throwable ex) {
+				System.out.println("Unable to send message=[" + msg + "] due to : " + ex.getMessage());
+			}
+		});
 	}
 }
+
+
+/*
+* Kafka is a fast stream processing platform. Therefore, it's better to handle the results asynchronously so that the subsequent messages do not wait for the result of the previous message. We can do this through a ListenableFuture:
+*
+*/
